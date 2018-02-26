dee0a738323bbd829c20e1a7ad92991c2237cfc5
rditon <robertditon@yahoo.co.uk>
2018-02-26 20:00:39 +0000

    Finish Jumpstart project

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,13 +11,16 @@ $('document').ready(function(){
     pauseOnFocus: false,
   });
 
-  var scrollDown = 0;
-  $(window).scroll(function(event){
-    var scroll = $(this).scrollTop();
-    if (scroll - scrollDown > 50){
-      var navBar =
-      $('.navbar').css('height');
-      $('.navbar').animate({top: '-' + z}, 150);
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
     }
   });
 });
